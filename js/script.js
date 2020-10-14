e26a81cac4f3d392de46d0790647691b2abf1085
Patrick Bello <mayorbello@yahoo.com>
2020-10-14 17:58:12 -0600

    used event listener for js and added to .gitignore

--- js/script.js
+++ js/script.js
@@ -3,14 +3,21 @@ const heights = window.innerHeight;
 const content = document.querySelector(".wrapper")
 
 // Declare function
-function resize() {
+function resizeHandler() {
   content.style.height = heights + "px";
 }
 
+// Add event listener that fires on window resize
+window.addEventListener('resize', resizeHandler());
+
+
+// Another method
 // Call/invoke function
-resize();
+// resize();
 
 // This fires when the document window resizes
-window.onresize = function() {
-   resize();
- };
+// window.onresize = function() {
+//    resize();
+//  };
+
+
